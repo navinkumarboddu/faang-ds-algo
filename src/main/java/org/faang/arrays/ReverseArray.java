885f3562f181c1a940b2880e91d5a4ce53c899eb
navinkumar <boddunavin2008@gmail.com>
2023-01-24 21:58:19 +0530

    Close #1

--- src/main/java/org/faang/arrays/ReverseArray.java
+++ src/main/java/org/faang/arrays/ReverseArray.java
@@ -2,6 +2,21 @@ package org.faang.arrays;
 
 import java.util.Arrays;
 
+/*
+Write a program to reverse an array or string.
+
+Given an array (or string), the task is to reverse the array/string.
+
+Examples :
+
+Input : arr[] = {1, 2, 3}
+Output : arr[] = {3, 2, 1}
+
+Input : arr[] = {4, 5, 1, 2}
+Output : arr[] = {2, 1, 5, 4}
+ */
+
+
 public class ReverseArray {
 
     public static void main(String[] args) {
